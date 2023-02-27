60e7ded6c7a9058c6b288142772c60f4fecda02b
Evandro Neris <evandroneris95@hotmail.com>
2023-02-27 00:06:15 -0300

    feat: Repository

---
+++ src/infra/repository/sequelize/toxicologicalSample.repository.ts
@@ -0,0 +1,48 @@
+import ToxicologicalSample from '../../../domain/toxicological-sample/entity/toxicologicalSample'
+import ToxicologialSampleRepositoryInterface from '../../../domain/toxicological-sample/repository/toxicologicalSample-repository.interface'
+import ToxicologicalSampleModel from './toxicologicalSample.model'
+
+export default class ToxicologicalSampleRepository implements ToxicologialSampleRepositoryInterface {
+  async create(entity: ToxicologicalSample): Promise<void> {
+    await ToxicologicalSampleModel.create({
+      id: entity.id,
+      sampleCode: entity.sampleCode,
+      cocaine: entity.cocaine,
+      amphetamine: entity.amphetamine,
+      methamphetamine: entity.methamphetamine,
+      mda: entity.mda,
+      mdma: entity.mdma,
+      thc: entity.thc,
+      morphine: entity.morphine,
+      codeine: entity.codeine,
+      heroine: entity.heroine,
+      benzoylecgonine: entity.benzoylecgonine,
+      cocaethylene: entity.cocaethylene,
+      norcocaine: entity.norcocaine,
+      sampleResult: entity.sampleResult
+    })
+  }
+
+  async findOne(_sampleCode: string): Promise<ToxicologicalSample> {
+    const result = await ToxicologicalSampleModel.findOne({ where: { sampleCode: _sampleCode }})
+
+    if (!result?.id) {
+      throw new Error('Toxicological Sample not found')
+    }
+
+    const { dataValues: { id, sampleCode, ...rest } } = result
+
+    const toxicologicalSample = new ToxicologicalSample(id, sampleCode)
+    toxicologicalSample.loadAllToxins(rest)
+    return toxicologicalSample
+  }
+
+  async findAll(): Promise<ToxicologicalSample[]> {
+    const toxicologicalSamleModels = await ToxicologicalSampleModel.findAll()
+    return toxicologicalSamleModels.map(({ dataValues: { id, sampleCode, ...rest } }: any) => {
+      const toxicologicalSample = new ToxicologicalSample(id, sampleCode)
+      toxicologicalSample.loadAllToxins(rest)
+      return toxicologicalSample
+    })
+  }
+}
---
+++ src/infra/repository/sequelize/user.repository.ts
@@ -0,0 +1,23 @@
+import User from '../../../domain/user/entity/user'
+import UserRepositoryInterface from '../../../domain/user/repository/user-repository.interface'
+import UserModel from './user.model'
+
+export default class UserRepository implements UserRepositoryInterface {
+  async create(entity: User): Promise<void> {
+    await UserModel.create({
+      id: entity.id,
+      email: entity.email,
+      password: entity.password
+    })
+  }
+
+  async findOne(_email: string): Promise<User> {
+    const result = await UserModel.findOne({ where: { email: _email } })
+    if (!result) {
+      throw new Error('User not found')
+    }
+
+    const { dataValues: { id, email, password } } = result
+    return new User(id, email, password)
+  }
+}
