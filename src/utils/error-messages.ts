b897b6b45da24f8b7a06e8ffdc324a2727718ceb
Evandro Neris <evandroneris95@hotmail.com>
2023-02-27 00:02:29 -0300

    feat: Utils

---
+++ src/utils/error-messages.ts
@@ -0,0 +1,12 @@
+const ErrorMessage = {
+  sampleCode: {
+    '0001': 'Sample Code is required',
+    '0002': 'Maximum 8 digits reached',
+    '0003': 'Special characters are not allowed'
+  },
+  toxinBase: {
+    '0001': 'value needs to be positive'
+  }
+}
+
+export default ErrorMessage
---
+++ src/utils/shared-constants.ts
@@ -0,0 +1,9 @@
+const SPECIAL_CHARACTERS= /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi
+const POSITIVE = 'positive'
+const NEGATIVE = 'negative'
+
+export {
+  SPECIAL_CHARACTERS,
+  POSITIVE,
+  NEGATIVE
+}
